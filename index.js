3097db070be8bb0ca0b34a7328ce2171d50a4160
Pranjal Kesarwani <89082511+PranjalKesarwani@users.noreply.github.com>
2023-08-21 00:01:10 +0530

    cors is working perfectly

--- index.js
+++ index.js
@@ -13,15 +13,12 @@ var cors = require('cors');
 
 const app = express();
 
-app.use(cors({ origin: true, credentials: true }));
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://sayhiii.netlify.app/");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    next();
-});
+app.use(cors({
+    origin: 'http://127.0.0.1:5173', // Update this line
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/user', userRoutes);
